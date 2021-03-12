7d8fdc1d235153d9fee65cdfc77bd0c6c90afcd4
Daniel Mark Low <daniel.m.low@gmail.com>
2021-03-11 19:40:46 -0500

    file_extension arg

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import argparse
-
+from distutils.util import strtobool
 # Modify default values
-sampling_rate = 22050
+sampling_rate = 16000
 bit_rate = '64k'
 feature_level = 'func'  # or 'lld' for matrix
 
@@ -9,11 +9,18 @@ feature_level = 'func'  # or 'lld' for matrix
 parser = argparse.ArgumentParser(description='Extract OpenSmile features')
 
 parser.add_argument('input_dir', type=str,
-                    help='path to directory of audios')
+                    help='path to directory of audios. Required. ')
 
 # Optional positional argument
 parser.add_argument('--output_dir', type=str,nargs='?',
-                    help='path to output for features')
+                    help='path to output for features or output to current dir')
+
+parser.add_argument('--wav_conversion', type=str,nargs='?',
+                    help='True or False. Default False because OpenSmile can handle most audio common files')
+
+parser.add_argument('--file_extension', type=str,nargs='?',
+                    help='wav, au, mp3, etc. default None.')
+
 
 args = parser.parse_args()
 
@@ -24,5 +31,18 @@ input_dir = args.input_dir
 output_dir = args.output_dir
 if output_dir == None:
 	output_dir = input_dir+'../' #default
+
+
+
+wav_conversion = args.wav_conversion
+if wav_conversion == None:
+	wav_conversion = True
+else:
+	wav_conversion = strtobool(args.wav_conversion)
+
+
+
+file_extension = args.file_extension
+
 
 
--- main.py
+++ main.py
@@ -26,30 +26,54 @@ if __name__ == "__main__":
 	# Config
 	input_dir = config.input_dir
 	output_dir = config.output_dir
+	wav_conversion = config.wav_conversion
 	sampling_rate = config.sampling_rate
 	bit_rate = config.bit_rate
 	feature_level = config.feature_level
+	file_extension = config.file_extension
 
 	# run
 	# ========================================
-	audio_filenames = os.listdir(input_dir)
-	audio_filenames = [n for n in audio_filenames if not n.startswith('.')]
+	if file_extension:
+		audio_filenames = list(Path(input_dir).rglob(f"*.{file_extension}"))
+	else:
+		audio_filenames = list(Path(input_dir).rglob(f"*"))
+	
+	audio_filenames = [str(n) for n in audio_filenames]
+	audio_filenames = [filepath for filepath in audio_filenames if not any(word in filepath for word in ['.DS_Store'])]
+	print(audio_filenames)
 
 
-	# Convert to wav and output in this dir
-	wav_dir = input_dir[:-1] + f'_wavs-{sampling_rate}fs-{bit_rate}/'
-	try: os.mkdir(wav_dir)
-	except: pass
+	
+	try: 
+		os.mkdir(output_dir)
+		print('created output_dir')
+	except: 
+		print('did not create output_dir')
+		pass
 
-	for filename in audio_filenames:
-		print(filename)
-		convert_to_wav.to_wav(input_dir=input_dir, filename=filename, output_dir=wav_dir,
-		       output_filename=filename[:-4] + '.wav',
-		       sampling_rate=sampling_rate, bit_rate=bit_rate)
+	if wav_conversion:
+		# Convert to wav and output in this dir
+		wav_dir = input_dir[:-1] + f'_wavs-{sampling_rate}fs-{bit_rate}/'
+
+		try: os.mkdir(wav_dir)
+		except: pass
+
+		for filename in audio_filenames:
+			print(filename)
+			convert_to_wav.to_wav(input_dir=input_dir, filename=filename, output_dir=wav_dir,
+			       output_filename=filename[:-4] + '.wav',
+			       sampling_rate=sampling_rate, bit_rate=bit_rate)
 
 
+		paths_to_audio = list(Path(wav_dir).rglob("*.[wW][aA][vV]"))
+	else:
+		paths_to_audio = list(Path(input_dir).rglob("*"))
+		paths_to_audio = [str(n) for n in paths_to_audio]
+		paths_to_audio = [n for n in paths_to_audio if n not in ['.DS_Store']]
 
-	paths_to_audio = list(Path(wav_dir).rglob("*.[wW][aA][vV]"))
+
+	
 
 	# Make sure all wavs have been converted
 	# https://stackoverflow.com/questions/16196712/python-to-wait-for-shell-command-to-complete
@@ -62,12 +86,9 @@ if __name__ == "__main__":
 		feature_set=opensmile.FeatureSet.eGeMAPSv02, #or path to conf: 'gemaps/eGeMAPSv02.conf'
 		feature_level=feature_level,
 	)
-	vectors = smile.process_files(paths_to_audio)
 
-	try:
-		os.mkdir(output_dir)
-	except:
-		pass
+	
+	vectors = smile.process_files(paths_to_audio)
 
 
 	output_path = output_dir+ f"{input_dir.split('/')[-2]}_eGeMAPSv02_{feature_level}_{str(datetime.datetime.now()).replace(' ', '-').replace(':', '-').replace('.', '-')}.csv"
